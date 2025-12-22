d011d56ad1dd2c4fbc958e885cf08439b47d7848
xcodeleon <evglvov5@gmail.com>
2025-12-22 11:27:45 +0300

    Initial commit

---
+++ src/main/java/com/xcodeleon/userapp4/UserApp4Application.java
@@ -0,0 +1,13 @@
+package com.xcodeleon.userapp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApp4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApp4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/xcodeleon/userapp4/UserApp4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcodeleon.userapp4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApp4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
